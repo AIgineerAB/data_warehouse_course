016e150256b8f796a073c44d1eacc9f7a6657aec
Db-Lau <debbielau.hk@gmail.com>
2025-06-25 15:03:49 +0200

    update dbt doc

--- 14_dbt_documentation/dbt_code/models/fct/fct_job_ads.sql
+++ 14_dbt_documentation/dbt_code/models/fct/fct_job_ads.sql
@@ -1,24 +1,10 @@
-WITH ja AS (SELECT * FROM {{ ref('src_job_ads') }}),
+-- this is an extract of the model
 
-jd AS (SELECT * FROM {{ ref('src_job_details') }}),
+with job_ads as (select * from {{ ref('src_job_ads') }})
 
-e AS (SELECT * FROM {{ ref('src_employer') }})
-
-SELECT 
-    {{dbt_utils.generate_surrogate_key(['jd.id', 'jd.headline'])}} AS job_details_key,
-    {{dbt_utils.generate_surrogate_key(['e.id', 'e.employer_name'])}} AS employer_key,
-    {# TODO: key to junk dimension #}
-    relevance, -- if null, aggregation functions can handle it properly
-    coalesce(vacancies, 1) as vacancies,
+select
+    {{ dbt_utils.generate_surrogate_key(['occupation__label']) }} as occupation_id,
+    vacancies,
+    relevance,
     application_deadline
-    -- for verifying FK relationships work
-    {# jd.id,
-    e.workplace_city,
-    e.employer_name,
-    jd.description #}
-FROM 
-    ja 
-LEFT JOIN 
-    jd ON ja.id = jd.id
-LEFT JOIN 
-    e ON ja.id = e.id
+from job_ads
--- 14_dbt_documentation/dbt_code/models/src/src_employer.sql
+++
@@ -1,9 +0,0 @@
-
-WITH stg_job_ads AS (SELECT * FROM {{ source('job_ads', 'stg_data_ads') }})
-
-SELECT
-    id,
-    employer__name AS employer_name,
-    employer__workplace AS employer_workplace,
-    workplace_address__city AS workplace_city
-FROM stg_job_ads
